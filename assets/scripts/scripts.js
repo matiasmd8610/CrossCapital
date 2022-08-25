d891136b45eeef8d748422391b28e3ea0e6fd4ec
Matias Di Iorio <matiasmd8610@gmail.com>
2022-08-24 22:27:09 -0300

    updates

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,19 @@
+//Sticky header
+window.addEventListener('scroll', function () {
+    var header = document.querySelector('header');
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+//SCROLL INDICATOR
+window.addEventListener('scroll', () => {
+    let scrollPosition = document.documentElement.scrollTop;
+    let maxHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let totalScrolled = scrollPosition * 100 / maxHeight;
+
+    document.querySelector("#scroll-indicator span").style.width = totalScrolled + '%';
+    if (scrollPosition > 5) {
+        document.querySelector("#scroll-indicator").style.backgroundColor = "rgba(21, 63, 89, 0.75)";
+    } else {
+        document.querySelector("#scroll-indicator").style.backgroundColor = "transparent";
+    }
+});
